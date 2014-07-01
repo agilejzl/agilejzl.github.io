66c7b98276e354e84f6f9bd2538a21f93b7bc9b6
Teddy Jiang <agilejzl@gmail.com>
2014-07-01 17:39:40 +0800

    Add Woopra Code

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,16 @@ $(function(){
     s.parentNode.insertBefore(hm, s);
   })();
   
+  // Woopra Code
+  (function(){
+      var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
+  })("woopra");
+  
+  woopra.config({
+      domain: 'agilejzl.github.io'
+  });
+  woopra.track();
+  
   // async code for Google Analytics
   (function(i,s,o,g,r,a,m){
     i['GoogleAnalyticsObject']=r;
